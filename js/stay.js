dbab2c3386fd491548fd294a8398970970520928
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-18 21:44:55 +0800

    首页 stay 优化

--- js/stay.js
+++ js/stay.js
@@ -1,10 +1,10 @@
-var time = new Timer();
+var $ = document.querySelector.bind(document), time = new Timer;
 function refreshStay(time, stayHour, stayMin, staySec) { var stay = time.timing(); stayHour.innerText = stay[0]; stayMin.innerText = stay[1]; staySec.innerText = stay[2]; }
-window.addEventListener('load', function () {
-	var stay = document.querySelector('#stay');
+addEventListener('load', function () {
+	var stay = $('#stay');
 	if (stay !== null) {
 		stay.innerHTML = '您已在本页停留 <span id="stayHour">0</span>时<span id="stayMin">0</span>分<span id="staySec">0</span>秒<button id="resetStay">重置计时</button>';
-		var stayHour = document.querySelector('#stayHour'), stayMin = document.querySelector('#stayMin'), staySec = document.querySelector('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, time, stayHour, stayMin, staySec);
-		document.querySelector('#resetStay').onclick = function () { time.reset(); refreshStay(time, stayHour, stayMin, staySec); };
+		var stayHour = $('#stayHour'), stayMin = $('#stayMin'), staySec = $('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, time, stayHour, stayMin, staySec);
+		$('#resetStay').onclick = function () { time.reset(); refreshStay(time, stayHour, stayMin, staySec); };
 	}
 });
