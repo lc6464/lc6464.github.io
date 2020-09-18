7d9b83ab78cc827916157a2fa7c5d4a31d4dc2e2
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-18 21:51:21 +0800

    js bug 修复

--- js/stay.js
+++ js/stay.js
@@ -1,10 +1,10 @@
 var $ = document.querySelector.bind(document), time = new Timer;
-function refreshStay(time, stayHour, stayMin, staySec) { var stay = time.timing(); stayHour.innerText = stay[0]; stayMin.innerText = stay[1]; staySec.innerText = stay[2]; }
+function refreshStay(time, stayDay, stayHour, stayMin, staySec) { var stay = time.timing(); stayDay.innerText = stay[0]; stayHour.innerText = stay[1]; stayMin.innerText = stay[2]; staySec.innerText = stay[3]; }
 addEventListener('load', function () {
 	var stay = $('#stay');
 	if (stay !== null) {
-		stay.innerHTML = '您已在本页停留 <span id="stayHour">0</span>时<span id="stayMin">0</span>分<span id="staySec">0</span>秒<button id="resetStay">重置计时</button>';
-		var stayHour = $('#stayHour'), stayMin = $('#stayMin'), staySec = $('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, time, stayHour, stayMin, staySec);
+		stay.innerHTML = '您已在本页停留 <span id="stayDay">0</span>天<span id="stayHour">0</span>时<span id="stayMin">0</span>分<span id="staySec">0</span>秒<button id="resetStay">重置计时</button>';
+		var stayDay = $('#stayDay'), stayHour = $('#stayHour'), stayMin = $('#stayMin'), staySec = $('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, time, stayDay, stayHour, stayMin, staySec);
 		$('#resetStay').onclick = function () { time.reset(); refreshStay(time, stayHour, stayMin, staySec); };
 	}
 });
