38a0e5ff974e829fe29cd27be2fc114618e337b5
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-19 10:58:30 +0800

    js bug fix

--- js/stay.js
+++ js/stay.js
@@ -1,10 +1,10 @@
-var $ = document.querySelector.bind(document), time = new Timer;
-function refreshStay(time, stayDay, stayHour, stayMin, staySec) { var stay = time.timing(); stayDay.innerText = stay[0]; stayHour.innerText = stay[1]; stayMin.innerText = stay[2]; staySec.innerText = stay[3]; }
+var $ = document.querySelector.bind(document), timer = new Timer;
+function refreshStay(timer, stayE) { var stay = timer.timing(); stayE[0].innerText = stay[0]; stayE[1].innerText = stay[1]; stayE[2].innerText = stay[2]; stayE[3].innerText = stay[3]; }
 addEventListener('load', function () {
-	var stay = $('#stay');
-	if (stay !== null) {
-		stay.innerHTML = '您已在本页停留 <span id="stayDay">0</span>天<span id="stayHour">0</span>时<span id="stayMin">0</span>分<span id="staySec">0</span>秒<button id="resetStay">重置计时</button>';
-		var stayDay = $('#stayDay'), stayHour = $('#stayHour'), stayMin = $('#stayMin'), staySec = $('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, time, stayDay, stayHour, stayMin, staySec);
-		$('#resetStay').onclick = function () { time.reset(); refreshStay(time, stayHour, stayMin, staySec); };
+	var stayEle = $('#stay');
+	if (stayEle !== null) {
+		stayEle.innerHTML = '您已在本页停留 <span id="stayDay">0</span>天<span id="stayHour">0</span>时<span id="stayMin">0</span>分<span id="staySec">0</span>秒<button id="resetStay">重置计时</button>';
+		var stayDay = $('#stayDay'), stayHour = $('#stayHour'), stayMin = $('#stayMin'), staySec = $('#staySec'); window.stayRefresh = setInterval(refreshStay, 500, timer, [stayDay, stayHour, stayMin, staySec]);
+		$('#resetStay').onclick = function () { timer.reset(); refreshStay(timer, [stayDay, stayHour, stayMin, staySec]); };
 	}
 });
