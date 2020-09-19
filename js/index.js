a5e183bd5ee565b20bed873757886c82651eec72
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-19 16:37:59 +0800

    js 优化

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-(function($){
+(function ($) {
 	function getUrl(subURL) { return '//cdn.jsdelivr.net/gh/lc6464/lc6464.github.io@master/' + subURL; }
 	function getComUrl(subURL) { return getUrl(`components/${subURL}.html`); }
 	if ($) {
 		var $ = document.querySelector.bind(document), $$ = document.querySelectorAll.bind(document);
+		Element.prototype.attr = function (name, value) { if (name == null) return undefined; else if (value == null) return this.getAttribute(name); else { this.setAttribute(name, value); return this; } };
 		function getCom(q, sURL) { if ($$(q)) { var x = new XMLHttpRequest; x.open('get', getComUrl(sURL)); x.onload = function () { var s = this; $$(q).forEach(e => e.innerHTML = s.responseText); }; x.send(); } }
 		$$('abbr').forEach(ele => ele.addEventListener('click', _ => alert(ele.innerText + '\r\n' + ele.title)));
-		$$('svg').forEach(ele => { ele.setAttribute('xmlns', 'http://www.w3.org/2000/svg'); ele.setAttribute('version', '1.1'); });
-		$$('svg.lc-icon').forEach(ele => ele.setAttribute('viewBox', '0 0 256 256'));
+		$$('svg').forEach(ele => { ele.attr('xmlns', 'http://www.w3.org/2000/svg').attr('version', '1.1'); });
+		$$('svg.lc-icon').forEach(ele => ele.attr('viewBox', '0 0 256 256'));
 	} else {
 		function getCom(query, subURL) { if ($(query).length) { $.ajax(getComUrl(subURL)).done(function (r) { $(query).html(r); }); } }
 		$('abbr').click(function () { alert(this.innerText + '\r\n' + this.title) });
@@ -15,5 +16,5 @@
 	}
 	getCom('.cr', 'copyright');
 	getCom('.license', 'license');
-	getCom('svg.lc-icon', 'lc.svg');
+	getCom('svg.lc-icon', 'lc-icon.svg');
 })(typeof jQuery === 'function' ? jQuery : undefined);
