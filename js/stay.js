7f48db3ef0bf3976253f604756f33eda9bc76dd2
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-19 18:03:55 +0800

    fix: js bug

--- js/stay.js
+++ js/stay.js
@@ -7,7 +7,7 @@
 			var htmlStr = '您已停留 <span id="sDay">0</span>天<span id="sHour">0</span>时<span id="sMin">0</span>分<span id="sSec">0</span>秒<button id="rStay">重置计时</button>';
 			hasJQuery ? e.html(htmlStr) : e.innerHTML = htmlStr;
 			var stayEs = [$('#sDay'), $('#sHour'), $('#sMin'), $('#sSec')]; window.stayRefresh = setInterval(refreshStay, 500, timer, stayEs);
-			$('#rStay').onclick = function () { timer.reset(); refreshStay(timer, stayEs); };
+			hasJQuery ? $('#rStay').click(function () { timer.reset(); refreshStay(timer, stayEs); }) : $('#rStay').onclick = function () { timer.reset(); refreshStay(timer, stayEs); };
 		}
 	});
 })((typeof jQuery === 'function' ? jQuery : document.querySelector.bind(document)), typeof jQuery === 'function');
