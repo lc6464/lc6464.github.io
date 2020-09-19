a6e23eb2418a0c08e4eddc9603208f30a0a63262
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-09-19 15:49:06 +0800

    js bug fix

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-function getUrl(subURL) { return '//cdn.jsdelivr.net/gh/lc6464/lc6464.github.io@master/' + subURL; }
-function getComUrl(subURL) { return getUrl(`components/${subURL}.html`); }
-if (typeof jQuery === 'undefined') {
-	var $ = document.querySelector.bind(document), $$ = document.querySelectorAll.bind(document);
-	function getCom(q, sURL) { if ($$(q)) { var x = new XMLHttpRequest; x.open('get', getComUrl(sURL)); x.onload = function () { var s = this; $$(q).forEach(e => e.innerHTML = s.responseText); }; x.send(); } }
-	$$('abbr').forEach(ele => ele.addEventListener('click', _ => alert(ele.innerText + '\r\n' + ele.title)));
-	$$('svg').forEach(ele => { ele.setAttribute('xmlns', 'http://www.w3.org/2000/svg'); ele.setAttribute('version', '1.1'); });
-	$$('svg.ic-icon').forEach(ele => ele.setAttribute('viewBox', '0 0 256 256'));
-} else {
-	function getCom(query, subURL) { if ($(query).length) { $.ajax(getComUrl(subURL)).done(function (r) { $(query).html(r); }); } }
-	$('abbr').click(function () { alert(this.innerText + '\r\n' + this.title) });
-	$('svg').attr('xmlns', 'http://www.w3.org/2000/svg').attr('version', '1.1');
-	$('svg.ic-icon').attr('viewBox', '0 0 256 256');
-}
-getCom('.cr', 'copyright');
-getCom('.license', 'license');
-getCom('svg.ic-icon', 'lc.svg');
+(function($){
+	function getUrl(subURL) { return '//cdn.jsdelivr.net/gh/lc6464/lc6464.github.io@master/' + subURL; }
+	function getComUrl(subURL) { return getUrl(`components/${subURL}.html`); }
+	if ($) {
+		var $ = document.querySelector.bind(document), $$ = document.querySelectorAll.bind(document);
+		function getCom(q, sURL) { if ($$(q)) { var x = new XMLHttpRequest; x.open('get', getComUrl(sURL)); x.onload = function () { var s = this; $$(q).forEach(e => e.innerHTML = s.responseText); }; x.send(); } }
+		$$('abbr').forEach(ele => ele.addEventListener('click', _ => alert(ele.innerText + '\r\n' + ele.title)));
+		$$('svg').forEach(ele => { ele.setAttribute('xmlns', 'http://www.w3.org/2000/svg'); ele.setAttribute('version', '1.1'); });
+		$$('svg.ic-icon').forEach(ele => ele.setAttribute('viewBox', '0 0 256 256'));
+	} else {
+		function getCom(query, subURL) { if ($(query).length) { $.ajax(getComUrl(subURL)).done(function (r) { $(query).html(r); }); } }
+		$('abbr').click(function () { alert(this.innerText + '\r\n' + this.title) });
+		$('svg').attr('xmlns', 'http://www.w3.org/2000/svg').attr('version', '1.1');
+		$('svg.ic-icon').attr('viewBox', '0 0 256 256');
+	}
+	getCom('.cr', 'copyright');
+	getCom('.license', 'license');
+	getCom('svg.ic-icon', 'lc.svg');
+})(typeof jQuery === 'function' ? jQuery : undefined);
--- js/stay.js
+++ js/stay.js
@@ -1,10 +1,13 @@
-var $ = document.querySelector.bind(document), timer = new Timer;
-function refreshStay(timer, stayEs) { var stay = timer.timing(); for (var i = 0; i < 4; i++) { stayEs[i].innerText = stay[i]; } }
-addEventListener('load', function () {
-	var e = $('#stay');
-	if (e !== null) {
-		e.innerHTML = '您已停留 <span id="sDay">0</span>天<span id="sHour">0</span>时<span id="sMin">0</span>分<span id="sSec">0</span>秒<button id="rStay">重置计时</button>';
-		var stayEs = [$('#sDay'), $('#sHour'), $('#sMin'), $('#sSec')]; window.stayRefresh = setInterval(refreshStay, 500, timer, stayEs);
-		$('#rStay').onclick = function () { timer.reset(); refreshStay(timer, stayEs); };
-	}
-});
+(function ($, hasJQuery) {
+	var timer = new Timer;
+	function refreshStay(timer, stayEs) { var stay = timer.timing(); for (var i = 0; i < 4; i++) { hasJQuery ? stayEs[i].text(stay[i]) : stayEs[i].innerText = stay[i]; } }
+	addEventListener('load', function () {
+		var e = $('#stay');
+		if (e !== null) {
+			var htmlStr = '您已停留 <span id="sDay">0</span>天<span id="sHour">0</span>时<span id="sMin">0</span>分<span id="sSec">0</span>秒<button id="rStay">重置计时</button>';
+			hasJQuery ? e.html(htmlStr) : e.innerHTML = htmlStr;
+			var stayEs = [$('#sDay'), $('#sHour'), $('#sMin'), $('#sSec')]; window.stayRefresh = setInterval(refreshStay, 500, timer, stayEs);
+			$('#rStay').onclick = function () { timer.reset(); refreshStay(timer, stayEs); };
+		}
+	});
+})((typeof jQuery === 'function' ? jQuery : document.querySelector.bind(document)), typeof jQuery === 'function');
